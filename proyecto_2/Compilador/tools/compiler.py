6e0eb43833cd4e6afacbaebd3f6d9f2e6e8ddb2f
Jonathana1106 <jonathana1196@gmail.com>
2022-10-27 20:10:26 -0600

    [INIT] compiler again

---
+++ proyecto_2/Compilador/tools/compiler.py
@@ -0,0 +1,48 @@
+# Operation Code
+
+op = {
+    'SUM': '',
+    'SUMI': '',
+    'RST': '',
+    'RSTI': '',
+    'MOV': '',
+    'MOVI': '',
+    'COM': '',
+    'COMI': '',
+    'GEM': '',
+    'CDM': '',
+    'SI': '',
+    'SIQ': '',
+    'SME': '',
+    'SMA': '',
+    'SMEI': '',
+    'SMAI': ''  
+}
+
+# Functions
+
+functions = {
+    'RR': {
+        'SUM': '',
+        'SUMI': '',
+        'RST': '',
+        'MOV': '',
+        'COM': ''
+    },
+    'RK': {
+        'SUMI': '',
+        'RSTI': '',
+        'MOVI': '',
+        'COMI': '',
+        'MOVI': ''
+    },
+    'MM': {
+        'GEM': '',
+        'CDM': ''
+    },
+    'IC': {
+        'RI': '',
+        'RIQ': '',
+        'RMQ': ''
+    }
+}
